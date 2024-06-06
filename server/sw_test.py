50be87c1acc78c18d7a46ac7049a882a7ca497e2
insectkim <kiminsect513@pukyong.ac.kr>
2024-06-06 13:47:47 +0900

    센서 테스트 코드 추가

---
+++ server/sw_test.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO 핀 설정
+sensor_pin = 21
+
+# GPIO 모드 설정
+GPIO.setmode(GPIO.BCM)
+# 내부 풀업 저항 활성화
+GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    while True:
+        if GPIO.input(sensor_pin):
+            print("문이 닫혔습니다!")
+        else:
+            print("문이 열렸습니다!")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("종료합니다.")
+    GPIO.cleanup()
